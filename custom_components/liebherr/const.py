bcaf81d9adce99638a644655f997cf33da1a9b91
Grant Ellsworth <gbellsworth@gmail.com>
2025-12-08 13:22:51 -0500

    Map presentation light brightness

--- custom_components/liebherr/const.py
+++ custom_components/liebherr/const.py
@@ -10,3 +10,6 @@ MIN_UPDATE_INTERVAL: Final[int] = 5  # in seconds
 MAX_UPDATE_INTERVAL: Final[int] = 300  # in seconds, 5 minutes
 
 CONF_POLL_INTERVAL: Final[str] = "poll_interval"
+
+BRIGHTNESS_SCALE: Final[dict[str, tuple[int, int]]] = {"WPgbi 7472-20": (1, 5)}
+DEFAULT_BRIGHTNESS_SCALE: Final[tuple[int, int]] = (1, 4)
--- custom_components/liebherr/light.py
+++ custom_components/liebherr/light.py
@@ -1,6 +1,5 @@
 """Support for Liebherr HydroBreeze."""
 
-import logging
 from typing import Any
 
 from pyliebherr import LiebherrControl, LiebherrDevice
@@ -11,10 +10,9 @@ from homeassistant.components.light import ATTR_BRIGHTNESS, ColorMode, LightEnti
 from homeassistant.core import HomeAssistant
 from homeassistant.util.color import value_to_brightness
 
+from .const import BRIGHTNESS_SCALE, DEFAULT_BRIGHTNESS_SCALE
 from .coordinator import LiebherrConfigEntry, LiebherrCoordinator
 from .entity import LiebherrEntity
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -31,9 +29,6 @@ async def async_setup_entry(
     async_add_entities(entities, update_before_add=True)
 
 
-BRIGHTNESS_SCALE = (1, 4)
-
-
 class LiebherrLight(LiebherrEntity, LightEntity):
     """Representation of a Liebherr switch entity."""
 
@@ -47,6 +42,9 @@ class LiebherrLight(LiebherrEntity, LightEntity):
         super().__init__(coordinator, device, control)
         self._attr_icon = "mdi:lightbulb"
         self._attr_supported_color_modes = {ColorMode.BRIGHTNESS}
+        self.brightness_scale: tuple[int, int] = BRIGHTNESS_SCALE.get(
+            device.model, DEFAULT_BRIGHTNESS_SCALE
+        )
 
     @property
     def available(self) -> bool:  # pyright: ignore[reportIncompatibleVariableOverride, reportIncompatibleMethodOverride]
@@ -58,7 +56,7 @@ class LiebherrLight(LiebherrEntity, LightEntity):
             if control.type == self._control.type:
                 if control.target is not None:
                     self._attr_brightness = value_to_brightness(
-                        BRIGHTNESS_SCALE, control.target
+                        self.brightness_scale, control.target
                     )
                     self._attr_is_on = control.target > 0
         self.async_write_ha_state()
@@ -69,7 +67,9 @@ class LiebherrLight(LiebherrEntity, LightEntity):
             control=PresentationLightControlRequest(brightness),
         )
         if brightness > 0:
-            self._attr_brightness = value_to_brightness(BRIGHTNESS_SCALE, brightness)
+            self._attr_brightness = value_to_brightness(
+                self.brightness_scale, brightness
+            )
             self._attr_is_on = True
         else:
             self._attr_is_on = False
@@ -82,7 +82,7 @@ class LiebherrLight(LiebherrEntity, LightEntity):
     ) -> None:
         """Turn the light on."""
         await self._async_on_off(
-            value_to_brightness(BRIGHTNESS_SCALE, kwargs[ATTR_BRIGHTNESS])
+            value_to_brightness(self.brightness_scale, kwargs[ATTR_BRIGHTNESS])
         )
 
     async def async_turn_off(self, **kwargs):
